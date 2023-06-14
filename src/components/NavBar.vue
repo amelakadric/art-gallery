222b1eda7c55a127061862d7c9f19a482ae36782
adjuricc <anjci011@gmail.com>
2023-06-15 01:31:27 +0200

    navbar w/o functional menu collapse + searchbar, not styled at all
    bc of search bar icon we need to:
    npm install @fortawesome/fontawesome-free

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,46 +1,80 @@
 <template>
-  <div>
-    <b-button v-b-toggle.sidebar-backdrop>Toggle Sidebar</b-button>
+  <aside class="sidebar">
+    <!-- button for clicking on collapsing menu -->
+    <a href="#" class="btn" id="dropdown-menu-btn">...</a>
 
-    <b-sidebar
-      id="sidebar-backdrop"
-      title="Sidebar with backdrop"
-      :backdrop-variant="variant"
-      backdrop
-      shadow
-    >
-      <div class="px-3 py-2">
-        <b-form-group label="Backdrop variant" label-for="backdrop-variant">
-          <b-form-select
-            id="backdrop-variant"
-            v-model="variant"
-            :options="variants"
-          ></b-form-select>
-        </b-form-group>
-      </div>
-    </b-sidebar>
-  </div>
+    <!-- search icon for collapsing search form -->
+    <search-bar id="search-bar"></search-bar>
+  </aside>
 </template>
 
+<style scoped>
+
+#search-bar{
+  margin-top: 200px;
+}
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 150px;
+  height: 100%;
+  background-color: blue;
+}
+
+#dropdown-menu-btn {
+  margin-top: 30px;
+}
+
+.btn:link,
+.btn:visited {
+ text-transform: uppercase;
+ text-decoration: none;
+ color: rgb(27, 27, 27);
+ padding: 10px 30px;
+ border: 1px solid;
+ border-radius: 1000px;
+ display: inline-block;
+ transition: all .2s;
+ position: relative;
+}
+
+.btn:hover {
+ transform: translateY(-5px);
+ box-shadow: 0 10px 20px rgba(27, 27, 27, .5);
+}
+
+.btn:active {
+ transform: translateY(-3px);
+}
+
+.btn::after {
+ content: "";
+ display: inline-block;
+ height: 100%;
+ width: 100%;
+ border-radius: 100px;
+ top: 0;
+ left: 0;
+ position: absolute;
+ z-index: -1;
+ transition: all .3s;
+} 
+
+.btn:hover::after {
+ background-color: rgb(0, 238, 255);
+ transform: scaleX(1.4) scaleY(1.5);
+ opacity: 0;
+}
+
+</style>
+
 <script>
+import SearchBar from './SearchBar.vue';
+
 export default {
-  name: "NavBar",
-  data() {
-    return {
-      variant: "dark",
-      variants: [
-        "transparent",
-        "white",
-        "light",
-        "dark",
-        "primary",
-        "secondary",
-        "success",
-        "danger",
-        "warning",
-        "info",
-      ],
-    };
-  },
+  components: { SearchBar },
+  name: "NavBar"
 };
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <b-button v-b-toggle.collapse-1 variant="primary"><i class="fa fa-search" id="search-icon"></i></b-button>
+        <b-collapse id="collapse-1" class="mt-2">
+            <div>
+                <b-form-input v-model="text" placeholder="Search" id="search-entry"></b-form-input>
+            </div>
+        </b-collapse>
+    </div>
+</template>
+
+<style scoped>
+    .fa.fa-search{
+    color: white;
+    }
+
+    #search-entry{
+        width:147px;
+    }
+</style>
+
+<script>
+export default{
+    name: "SearchBar"
+}
+</script>
