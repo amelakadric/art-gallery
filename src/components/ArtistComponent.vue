dfffa8d970fe8edd51b08cde713db6b7ae3c623b
adjuricc <anjci011@gmail.com>
2023-06-26 15:41:15 +0200

    update artists view, photo added

--- src/components/ArtistComponent.vue
+++ src/components/ArtistComponent.vue
@@ -1,22 +1,21 @@
 <template>
     <div id="artist">
         <b-card class="art-card">
-                <!-- <img src= {{myartist.photo}} alt="Image" class="card-image"> -->
+                <img :src="myartist.photo" alt="Image" class="card-image">
                 <div class="card-body">
                     <h5 class="card-title">{{myartist.name}}</h5>
                     <p class="card-text">{{myartist.bio}}</p>
                 </div>
         </b-card>
     </div>
-    <!-- <div id='artist'>
-        <hr/>
-        <b>Title:</b> {{myartist.name}}
-        <hr/>
-        <i>Author: {{myartist.bio}}
-        Photo url: {{myartist.photo}}
-        </i>
-    </div> -->
 </template>
+
+<style scoped>
+    img{
+        width: 100px;
+        height: 100px;
+    }
+</style>
 
 <script>
     export default {
