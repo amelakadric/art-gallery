36588993bbf2f6a2952b5075d416c82d862b8a7d
adjuricc <anjci011@gmail.com>
2023-07-04 19:53:04 +0200

    linked homepage

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="text-center">
     <div class="row">
+      
       <div class="col-md-6 mb-4">
+        <router-link to="/paintings">
         <b-card img-left class="text-left card-component">
           <img
             src="https://images.rawpixel.com/image_400/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGR2YW5nb2doLXNudmdyb2IuanBn.jpg"
@@ -10,38 +12,45 @@
           />
           <b-card-title class="mt-5"><h2>Slikarstvo</h2></b-card-title>
         </b-card>
+        </router-link>
       </div>
       <div class="col-md-6 mb-4">
-        <b-card img-left class="text-left card-component">
-          <img
-            src="https://images.unsplash.com/photo-1558611913-d707111c702e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2N1bHB0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-            alt="Image"
-            class="card-image"
-          />
-          <b-card-title class="mt-5"><h2>Vajarstvo</h2></b-card-title>
-        </b-card>
+        <router-link to="/sculptures">
+          <b-card img-left class="text-left card-component">
+            <img
+              src="https://images.unsplash.com/photo-1558611913-d707111c702e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2N1bHB0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+              alt="Image"
+              class="card-image"
+            />
+            <b-card-title class="mt-5"><h2>Vajarstvo</h2></b-card-title>
+          </b-card>
+        </router-link>
       </div>
     </div>
     <div class="row">
       <div class="col-md-6 mb-4">
-        <b-card img-left class="text-left card-component">
-          <img
-            src="https://images.unsplash.com/photo-1542460631-89dd12cd704c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGFyY2hpdGVjdHVyZSUyMG9sZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            alt="Image"
-            class="card-image"
-          />
-          <b-card-title class="mt-5"><h2>Arhitektura</h2></b-card-title>
-        </b-card>
+        <router-link to="/architecture">
+          <b-card img-left class="text-left card-component">
+            <img
+              src="https://images.unsplash.com/photo-1542460631-89dd12cd704c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGFyY2hpdGVjdHVyZSUyMG9sZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Image"
+              class="card-image"
+            />
+            <b-card-title class="mt-5"><h2>Arhitektura</h2></b-card-title>
+          </b-card>
+        </router-link>
       </div>
       <div class="col-md-6 mb-4">
-        <b-card img-left class="text-left card-component">
-          <img
-            src="https://images.unsplash.com/photo-1569783721854-33a99b4c0bae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YXJ0JTIwZ2FsbGVyeXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            alt="Image"
-            class="card-image"
-          />
-          <b-card-title class="mt-5"><h2>Izložba</h2></b-card-title>
-        </b-card>
+        <router-link to="/exhibition">
+          <b-card img-left class="text-left card-component">
+            <img
+              src="https://images.unsplash.com/photo-1569783721854-33a99b4c0bae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YXJ0JTIwZ2FsbGVyeXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Image"
+              class="card-image"
+            />
+            <b-card-title class="mt-5"><h2>Izložba</h2></b-card-title>
+          </b-card>
+        </router-link>
       </div>
     </div>
   </div>
@@ -58,6 +67,7 @@
   width: 35rem;
   height: 20rem;
   text-align: center;
+  text-decoration: none;
   
 }
 
