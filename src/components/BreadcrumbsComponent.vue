63c0a035fb290add84a1c7f2748ca299765b2cef
Amela Kadric <amela00108@gmail.com>
2023-07-06 00:06:57 +0200

    styled breadcrumbs

--- src/components/BreadcrumbsComponent.vue
+++ src/components/BreadcrumbsComponent.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
+<style scoped>
+p {
+  text-align: left;
+  margin-left: 1rem;
+  color: rgb(245, 216, 183);
+  font-style: italic;
+  text-shadow: 2px 1px 1px black;
+}
+</style>
+
 <script>
 export default {
   name: "BreadcrumbsComponent",
