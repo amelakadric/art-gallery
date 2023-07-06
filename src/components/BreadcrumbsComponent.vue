e0e47a76f31e732c050f7bc884357028ff1e8932
Amela Kadric <amela00108@gmail.com>
2023-07-06 12:31:43 +0200

    breadcrumbs with links

--- src/components/BreadcrumbsComponent.vue
+++ src/components/BreadcrumbsComponent.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
-    <p>{{ breadcrumb }}</p>
+    <p>
+      <a
+        v-for="breadcrumb of breadcrumbs"
+        :key="breadcrumb.link"
+        :href="breadcrumb.link"
+        ><span>></span>{{ breadcrumb.word }}</a
+      >
+    </p>
   </div>
 </template>
 
 <style scoped>
+a {
+  color: inherit;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+span,
+span:hover {
+  text-decoration: none;
+}
 p {
   text-align: left;
   margin-left: 1rem;
@@ -19,119 +37,257 @@ export default {
   name: "BreadcrumbsComponent",
   data() {
     return {
-      breadcrumb: "",
+      breadcrumbs: [],
     };
   },
   created() {
     console.log(this.$route.path);
     switch (this.$route.path) {
+      case "/":
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Početna", link: "/homesrb" });
+        break;
+      case "/srb":
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Početna", link: "/homesrb" });
+        break;
       case "/homesrb":
-        this.breadcrumb = "Početna";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Početna", link: "/homesrb" });
         break;
       case "/paintingssrb":
-        this.breadcrumb = "Početna>Slikarstvo";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" }
+        );
         break;
       case "/sculpturesrb":
-        this.breadcrumb = "Početna>Vajarstvo";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Vajarstvo", link: "/sculpturesrb" }
+        );
         break;
       case "/architecturessrb":
-        this.breadcrumb = "Početna>Arhitektura";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Arhitektura", link: "/architecturessrb" }
+        );
         break;
       case "/myAccountsrb":
-        this.breadcrumb = "Moj nalog";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Moj nalog", link: "/myAccountsrb" });
         break;
       case "/artistssrb":
-        this.breadcrumb = "Umetnici";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Umetnici", link: "/artistssrb" });
         break;
       case "/exhibitionsrb":
-        this.breadcrumb = "Izložba";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Izložba", link: "/exhibitionsrb" });
         break;
       case "/contactussrb":
-        this.breadcrumb = "Kontakt";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Kontakt", link: "/contactussrb" });
         break;
       case "/aboutusssrb":
-        this.breadcrumb = "O nama";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "O nama", link: "/aboutusssrb" });
+        break;
+      case "/eng":
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Home", link: "/eng" });
         break;
       case "/homeeng":
-        this.breadcrumb = "Home";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Home", link: "/homeeng" });
         break;
       case "/paintingseng":
-        this.breadcrumb = "Home>Paintings";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Paintings", link: "/paintingseng" }
+        );
         break;
       case "/sculptureseng":
-        this.breadcrumb = "Home>Sculptures";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Sculptures", link: "/sculptureseng" }
+        );
         break;
       case "/architectureseng":
-        this.breadcrumb = "Home>Architecture";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Architecture", link: "/architectureseng" }
+        );
         break;
       case "/myAccounteng":
-        this.breadcrumb = "My account";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "My account", link: "/myAccounteng" });
         break;
       case "/artistseng":
-        this.breadcrumb = "Artists";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Artists", link: "/artistseng" });
         break;
       case "/exhibitionseng":
-        this.breadcrumb = "Exhibition";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Exhibition", link: "/exhibitionseng" });
         break;
       case "/contactuseng":
-        this.breadcrumb = "Contact";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "Contact", link: "/exhibitionseng" });
         break;
       case "/aboutuseng":
-        this.breadcrumb = "About us";
+        this.breadcrumbs = [];
+        this.breadcrumbs.push({ word: "About us", link: "/aboutuseng" });
         break;
       case "/artworksrb/0":
-        this.breadcrumb = "Početna>Slikarstvo>Vrisak"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Vrisak", link: "/artworksrb/0" }
+        );
         break;
       case "/artworksrb/1":
-        this.breadcrumb = "Početna>Slikarstvo>Poslednja večera"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Poslednja večera", link: "/artworksrb/1" }
+        );
         break;
       case "/artworksrb/2":
-        this.breadcrumb = "Početna>Slikarstvo>Mona Liza"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Mona Liza", link: "/artworksrb/2" }
+        );
         break;
       case "/artworksrb3":
-        this.breadcrumb = "Početna>Vajarstvo>Bacač diska"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Bacač diska", link: "/artworksrb/3" }
+        );
         break;
       case "/artworksrb/4":
-        this.breadcrumb = "Početna>Vajarstvo>Mikelanđelov David"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Mikelanđelov David", link: "/artworksrb/4" }
+        );
         break;
       case "/artworksrb/5":
-        this.breadcrumb = "Početna>Vajarstvo>Miloska Venera"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Miloska Venera", link: "/artworksrb/5" }
+        );
         break;
       case "/artworksrb/6":
-        this.breadcrumb = "Početna>Arhitektura>La Sagrada Familia"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "La Sagrada Familia", link: "/artworksrb/6" }
+        );
         break;
       case "/artworksrb/7":
-        this.breadcrumb = "Početna>Arhitektura>Krivi toranj u Pizi"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Krivi toranj u Pizi", link: "/artworksrb/7" }
+        );
         break;
       case "/artworksrb/8":
-        this.breadcrumb = "Početna>Arhitektura>Taj Mahal"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Početna", link: "/homesrb" },
+          { word: "Slikarstvo", link: "/paintingssrb" },
+          { word: "Taj Mahal", link: "/artworksrb/8" }
+        );
         break;
       case "/artworkeng/0":
-        this.breadcrumb = "Home>Painting>The Scream"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "The Scream", link: "/artworkeng/0" }
+        );
         break;
       case "/artworkeng/1":
-        this.breadcrumb = "Home>Painting>The Last Supper"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "The Last Supper", link: "/artworkeng/1" }
+        );
         break;
       case "/artworkeng/2":
-        this.breadcrumb = "Home>Painting>Mona Lisa"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "Mona Lisa", link: "/artworkeng/2" }
+        );
         break;
       case "/artworkeng/3":
-        this.breadcrumb = "Home>Sculpture>Discus Thrower"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "Discus Thrower", link: "/artworkeng/3" }
+        );
         break;
       case "/artworkeng/4":
-        this.breadcrumb = "Home>Sculpture>David"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "David", link: "/artworkeng/4" }
+        );
         break;
       case "/artworkeng/5":
-        this.breadcrumb = "Home>Sculpture>Milos Venus"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "Milos Venus", link: "/artworkeng/5" }
+        );
         break;
       case "/artworkeng/6":
-        this.breadcrumb = "Home>Architecture>La Sagrada Familia"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "La Sagrada Familia", link: "/artworkeng/6" }
+        );
         break;
       case "/artworkeng/7":
-        this.breadcrumb = "Home>Architecture>The Leaning Tower of Pisa"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "The Leaning Tower of Pisa", link: "/artworkeng/7" }
+        );
         break;
       case "/artworkeng/8":
-        this.breadcrumb = "Home>Architecture>Taj Mahal"
+        this.breadcrumbs = [];
+        this.breadcrumbs.push(
+          { word: "Home", link: "/homeeng" },
+          { word: "Painting", link: "/paintingseng" },
+          { word: "Taj Mahal", link: "/artworkeng/8" }
+        );
         break;
     }
   },
